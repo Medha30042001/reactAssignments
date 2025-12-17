a5c395e8afd9b6a529fa126b35cead0ea2a73caa
Medha Adepu <medha.adepucse@gmail.com>
2025-12-17 23:30:26 +0530

    prps

---
+++ pass-props/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MessageCard from './MessageCard'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+       <MessageCard
+        title="Welcome"
+        message="This is my first reusable React component."
+      />
+
+      <MessageCard
+        title="React Props"
+        message="Props allow data to be passed from parent to child."
+      />
+
+      <MessageCard
+        title="Reusable Components"
+        message="Write once, reuse everywhere."
+      />
+
+      <MessageCard
+        title="Done!"
+        message="This card is also using the same component."
+      />
+    </>
+  )
+}
+
+export default App
