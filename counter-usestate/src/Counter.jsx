3e91a62a8a069c2d46f0dcbd2cd5a766ee4ef71f
Medha Adepu <medha.adepucse@gmail.com>
2025-12-17 21:10:28 +0530

    counter useState

---
+++ counter-usestate/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Counter(){
+
+    const [count, setCount] = useState(0);
+
+    function increaseCount(){
+        setCount(count + 1);
+    }
+
+    return(
+        <>
+            <h3>{count}</h3>
+            <button onClick={increaseCount}>Increase</button>
+        </>
+    );
+}
+
+export default Counter;
